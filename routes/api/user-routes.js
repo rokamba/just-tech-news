0199d7dabf7e74c300d86091af8db08fc0769998
rokamba <rokamba92@gmail.com>
2022-01-23 16:18:34 -0600

    Create the Login Route for Authentication

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -16,7 +16,6 @@ router.get('/', (req, res) => {
 });
 
 // GET /api/users/1
-// GET /api/users/1
 router.get('/:id', (req, res) => {
     User.findOne({
         attributes: { exclude: ['password'] },
@@ -25,6 +24,7 @@ router.get('/:id', (req, res) => {
         }
     })
       .then(dbUserData => {
+        
         if (!dbUserData) {
           res.status(404).json({ message: 'No user found with this id' });
           return;
@@ -50,6 +50,34 @@ router.post('/', (req, res) => {
         console.log(err);
         res.status(500).json(err);
       });
+});
+
+router.post('/login', (req, res) => {
+    // expects {email: 'lernantino@gmail.com', password: 'password1234'}
+      User.findOne({
+        where: {
+          email: req.body.email
+        }
+      }).then(dbUserData => {
+        if (!dbUserData) {
+          res.status(400).json({ message: 'No user with that email address!' });
+          return;
+        }
+    
+        // add comment syntax in front of this line in the .then()
+        // res.json({ user: dbUserData }
+    
+        // Verify 
+          const validPassword = dbUserData.checkPassword(req.body.password);
+          if (!validPassword) {
+              res.status(400).json({ message: 'Incorrect password!' });
+              return;
+          }
+
+          res.json({ user: dbUserData, message: 'You are now logged in!' });
+
+    
+    });  
 });
 
 // PUT /api/users/1
